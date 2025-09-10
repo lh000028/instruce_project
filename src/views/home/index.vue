5b82487d62ba72d4aa4483f30bfa9958cb830b48
liuhh <denglun10210328@163.com>
2025-09-10 16:17:21 +0800

    修改vue-config文件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    this is home
+    this is home test
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/instruce_project/' // 替换为您的仓库名
+    : '/'
+
 })
